d5e4c59bcbe59d02ce5274fee7864eca636df58f
lilanka <pathirage.lilanka@gmail.com>
2022-04-30 19:59:40 +0530

    evaluate changed

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,6 @@
-# load checkpoint and evaluating
+import argparse
 from os.path import join
 from functools import partial
-import argparse
 
 import torch
 from torch.utils.data import DataLoader
@@ -13,77 +12,63 @@ from utils import collate_fn
 from model import LatexProducer, Im2LatexModel
 from model.score import score_files
 
-
 def main():
+  parser = argparse.ArgumentParser(description="Evaluate the model")
+  parser.add_argument('--model_path', required=True, help='path of the evaluated model')
 
-    parser = argparse.ArgumentParser(description="Im2Latex Evaluating Program")
-    parser.add_argument('--model_path', required=True,
-                        help='path of the evaluated model')
+  # model args
+  parser.add_argument("--data_path", type=str, default="./sample_data/", help="The dataset's dir")
+  parser.add_argument("--cuda", action='store_true', default=True, help="Use cuda or not")
+  parser.add_argument("--batch_size", type=int, default=32)
+  parser.add_argument("--beam_size", type=int, default=5)
+  parser.add_argument("--result_path", type=str, default="./results/result.txt", help="The file to store result")
+  parser.add_argument("--ref_path", type=str, default="./results/ref.txt", help="The file to store reference")
+  parser.add_argument("--max_len", type=int, default=64, help="Max step of decoding")
+  parser.add_argument("--split", type=str, default="validate", help="The data split to decode")
 
-    # model args
-    parser.add_argument("--data_path", type=str,
-                        default="./sample_data/", help="The dataset's dir")
-    parser.add_argument("--cuda", action='store_true',
-                        default=True, help="Use cuda or not")
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--beam_size", type=int, default=5)
-    parser.add_argument("--result_path", type=str,
-                        default="./results/result.txt", help="The file to store result")
-    parser.add_argument("--ref_path", type=str,
-                        default="./results/ref.txt", help="The file to store reference")
-    parser.add_argument("--max_len", type=int,
-                        default=64, help="Max step of decoding")
-    parser.add_argument("--split", type=str,
-                        default="validate", help="The data split to decode")
+  args = parser.parse_args()
 
-    args = parser.parse_args()
+  checkpoint = torch.load(join(args.model_path))
+  model_args = checkpoint['args']
 
-    # 加载 模型
-    checkpoint = torch.load(join(args.model_path))
-    model_args = checkpoint['args']
+  vocab = load_vocab(args.data_path)
+  use_cuda = True if args.cuda and torch.cuda.is_available() else False
 
-    # 读入词典,设置其他相关参数
-    vocab = load_vocab(args.data_path)
-    use_cuda = True if args.cuda and torch.cuda.is_available() else False
+  data_loader = DataLoader(
+    Im2LatexDataset(args.data_path, args.split, args.max_len),
+    batch_size=args.batch_size,
+    collate_fn=partial(collate_fn, vocab.sign2id),
+    pin_memory=True if use_cuda else False,
+    num_workers=4
+  )
 
-    # 加载测试集
-    data_loader = DataLoader(
-        Im2LatexDataset(args.data_path, args.split, args.max_len),
-        batch_size=args.batch_size,
-        collate_fn=partial(collate_fn, vocab.sign2id),
-        pin_memory=True if use_cuda else False,
-        num_workers=4
-    )
+  model = Im2LatexModel(
+    len(vocab), model_args.emb_dim, model_args.dec_rnn_h,
+    add_pos_feat=model_args.add_position_features,
+    dropout=model_args.dropout
+  )
+  model.load_state_dict(checkpoint['model_state_dict'])
 
-    model = Im2LatexModel(
-        len(vocab), model_args.emb_dim, model_args.dec_rnn_h,
-        add_pos_feat=model_args.add_position_features,
-        dropout=model_args.dropout
-    )
-    model.load_state_dict(checkpoint['model_state_dict'])
+  out_file = open(args.result_path, 'w')
+  ref_file = open(args.ref_path, 'w')
 
-    result_file = open(args.result_path, 'w')
-    ref_file = open(args.ref_path, 'w')
+  latex_producer = LatexProducer(model, vocab, max_len=args.max_len, use_cuda=use_cuda, beam_size=args.beam_size)
 
-    latex_producer = LatexProducer(
-        model, vocab, max_len=args.max_len,
-        use_cuda=use_cuda, beam_size=args.beam_size)
+  for imgs, training_target, target_cal_loss in tqdm(data_loader):
+    try:
+      reference = latex_producer._idx2formulas(target_cal_loss)
+      results = latex_producer(imgs)
+    except RuntimeError:
+      break
 
-    for imgs, tgt4training, tgt4cal_loss in tqdm(data_loader):
-        try:
-            reference = latex_producer._idx2formulas(tgt4cal_loss)
-            results = latex_producer(imgs)
-        except RuntimeError:
-            break
+    out_file.write('\n'.join(results))
+    ref_file.write('\n'.join(reference))
 
-        result_file.write('\n'.join(results))
-        ref_file.write('\n'.join(reference))
+  score = score_files(args.result_path, args.ref_path)
+  print("beam search result:", score)
 
-    result_file.close()
-    ref_file.close()
-    score = score_files(args.result_path, args.ref_path)
-    print("beam search result:", score)
-
+  out_file.close()
+  ref_file.close()
 
 if __name__ == "__main__":
-    main()
+  main()
